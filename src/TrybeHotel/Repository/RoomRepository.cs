9a4076d80462b5d3a4d8ddedbe89aae0c531b954
DouglasDainese <douglas_dainese@hotmail.com>
2024-02-05 22:46:37 -0300

    Resolução req 7 - Cria rota post /room

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -1,5 +1,6 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrybeHotel.Repository
 {
@@ -14,16 +15,41 @@ namespace TrybeHotel.Repository
         // 6. Desenvolva o endpoint GET /room/:hotelId
         public IEnumerable<RoomDto> GetRooms(int HotelId)
         {
-            throw new NotImplementedException(); 
+            throw new NotImplementedException();
         }
 
         // 7. Desenvolva o endpoint POST /room
-        public RoomDto AddRoom(Room room) {
-            throw new NotImplementedException(); 
+        public RoomDto AddRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+
+            Room roomAdded = _context.Rooms
+            .Include(room => room.Hotel)
+            .ThenInclude(hotel => hotel.City)
+            .First(r => r.Name == room.Name);
+
+            return new RoomDto
+            {
+                RoomId = roomAdded.RoomId,
+                Name = roomAdded.Name,
+                Capacity = roomAdded.Capacity,
+                Image = roomAdded.Image,
+                Hotel = new HotelDto
+                {
+                    HotelId = roomAdded.HotelId,
+                    Name = roomAdded?.Hotel?.Name,
+                    Address = roomAdded?.Hotel?.Address,
+                    CityId = roomAdded?.Hotel?.CityId,
+                    CityName = roomAdded?.Hotel?.City?.Name
+                }
+            };
         }
 
+
         // 8. Desenvolva o endpoint DELETE /room/:roomId
-        public void DeleteRoom(int RoomId) {
+        public void DeleteRoom(int RoomId)
+        {
             throw new NotImplementedException();
         }
     }
